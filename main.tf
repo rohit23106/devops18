b828b75e42a445c19ce0d5b38bd1cef48fd7e7c2
rohit23106 <63347250+rohit23106@users.noreply.github.com>
2025-01-20 19:45:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-0ddc798b3f1a5117e"
+    image_id           = "ami-0ed194d7eff6d2f81"
     instance_type = "t2.micro"
-    key_name = "mustafakey-pair"
+    key_name = "keypair_monolithic"
     
 }
    
@@ -11,7 +11,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-0883b34c988bcbf20", "subnet-0f4f4322d734fcc38"]
+     subnets = ["subnet-02a736976786db807", "subnet-0f1e1fcf454c3bb5e"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
@@ -29,7 +29,7 @@ resource "aws_autoscaling_group" "web_server_asg" {
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    availability_zones    = ["us-east-1a", "us-east-1b"] 
+    availability_zones    = ["ap-south-1b", "ap-south-1a"] 
     launch_template {
         id      = aws_launch_template.web_server_as.id
         version = "$Latest"
