76736c93c5213263b06d1842422be4fb5fca39df
rohit23106 <63347250+rohit23106@users.noreply.github.com>
2025-01-21 19:31:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
     image_id           = "ami-0c867e3aca4358952"
+    vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t3.micro"
     key_name = "new_project_monolitic"
     
